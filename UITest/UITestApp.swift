d3a9e297cbd107dd0dc080002598b9dd91e2fd66
Kirill Ki <kirillkalapov@gmail.com>
2022-05-29 20:02:20 +0200

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by  Mr.Ki on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
